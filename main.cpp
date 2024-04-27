45269f01c3955151ad7c2700b8333b3826ac7f2f
Aaron & Kenneth <166048423+AaronAndKenneth@users.noreply.github.com>
2024-04-27 17:03:24 +0000

    added comment

--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@ void DisplaySales() {
     // We take information from calculate sales
     // Note there is no gross sales var
     // Display Gross, Day, Week, Month
+    // date sold is somehow correlated to time on lot
 
 }
