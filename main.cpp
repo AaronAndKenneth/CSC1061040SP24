14ce8b543f3e21b065cbf5aca31fc3ef8c1e6730
Aaron & Kenneth <166048423+AaronAndKenneth@users.noreply.github.com>
2024-04-30 16:00:45 +0000

    created displaysales

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
-void DisplaySales() {
-    // We take information from calculate sales
-    // Note there is no gross sales var
-    // Display Gross, Day, Week, Month
-    // date sold is somehow correlated to time on lot
+//input: data from calculate sales
+//process:
+//output: display to user gross sales for day, week, month
+#include <iostream>
+using namespace std;
+
+void DisplaySales(int salesDay, int salesWeek, int salesMonth) {
+    cout << "Sales today: $" << salesDay << endl;
+    cout << "Sales this week: $" << salesWeek << endl;
+    cout << "Sales this month: $" << salesMonth << endl;
+
+}
+
+int main() {
+    int salesDay, salesWeek, salesMonth = 0;
+    // Does calculateSales update above vars?
+    DisplaySales(salesDay, salesWeek, salesMonth);
+
+    return 1;
 
 }
